a59c4796495a38f7211973b245dfcb7f18a5e6c4
unknown <rishikeshmore5@gmail.com>
2023-10-02 06:24:57 +0530

    first commit4

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-          <Link to="/" className="link">
+          <Link to="/netflix" className="link">
             <span>Homepage</span>
           </Link>
           <Link to="/series" className="link">
